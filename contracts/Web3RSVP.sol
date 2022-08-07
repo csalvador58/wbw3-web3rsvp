ef59103d37658ed3ea2975fa146e0212e3d8021d
Salvador <csalvador58@gmail.com>
2022-08-06 21:04:34 -0700

    Defined event data structure

--- contracts/Web3RSVP.sol
+++ contracts/Web3RSVP.sol
@@ -4,6 +4,26 @@ pragma solidity ^0.8.9;
 // Import this file to use console.log
 import "hardhat/console.sol";
 
+
+contract Web3RSVP {
+    struct CreateEvent {
+        bytes32 eventId;
+        string eventDataCID;
+        address eventOwner;
+        uint256 eventTimestamp;
+        uint256 deposit;
+        uint256 maxCapacity;
+        address[] confirmedRSVPs;
+        address[] claimedRSVPs;
+        bool paidOut;
+    }
+
+    mapping(bytes32 => CreateEvent) public idToEvent;
+
+}
+
+
+/*
 contract Lock {
     uint public unlockTime;
     address payable public owner;
@@ -32,3 +52,4 @@ contract Lock {
         owner.transfer(address(this).balance);
     }
 }
+*/
